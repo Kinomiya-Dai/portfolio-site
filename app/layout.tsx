2bc19c6301dbbd3419443b34bf11d05e9c9114ec
Kinomiya-Dai <utsunomiya.hiroki49@gmail.com>
2025-11-19 00:01:03 +0900

    一度コミット

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,5 @@
 import './global.css'
 import type { Metadata } from 'next'
-import { GeistSans } from 'geist/font/sans'
-import { GeistMono } from 'geist/font/mono'
 import { Navbar } from './components/nav'
 import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
@@ -45,11 +43,9 @@ export default function RootLayout({
 }) {
   return (
     <html
-      lang="en"
+      lang="ja"
       className={cx(
-        'text-black bg-white dark:text-white dark:bg-black',
-        GeistSans.variable,
-        GeistMono.variable
+        'text-pf-text bg-pf-bg dark:text-pf-text dark:bg-pf-bg',
       )}
     >
       <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
