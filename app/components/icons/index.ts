dfb0414b0b4f71e85f222ea5c6157ec3890a02de
Kinomiya-Dai <utsunomiya.hiroki49@gmail.com>
2025-11-22 23:50:47 +0900

    postでsplideを使用しようとすると、"useclient"を宣言する際に、呼び出し先のファイルでfsを読み込む際にエラーになるため、分離する前の前処理

--- app/components/icons/index.ts
+++ app/components/icons/index.ts
@@ -1,4 +1,5 @@
 export { default as CpdaLogo } from "./cpda-logo";
 export { default as IpaLogo } from "./ipa-logo";
+export { default as UndrawCodeThinking } from "./undraw-code-thinking";
 export { default as UndrawManLight } from "./undraw-man-light";
 export { default as UndrawDevAvatarIcon } from "./undraw-dev-avatar-icon";
