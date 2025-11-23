48ed7141ffd6aff3dadabe4ae515cc48438dfe6d
Kinomiya-Dai <utsunomiya.hiroki49@gmail.com>
2025-11-24 03:08:43 +0900

    ・ローディング画面の実装 ・LP制作物エリアの実装

--- app/feature/work/components/displayPosts.tsx
+++ app/feature/work/components/displayPosts.tsx
@@ -3,10 +3,14 @@
 import Link from "next/link"
 
 import dynamic from "next/dynamic";
+import { FaReact } from "react-icons/fa";
+import { SiNextdotjs } from "react-icons/si";
+import Loading from "app/components/loading";
 
 // ssr: false でサーバーではレンダリングされない
 const Splide = dynamic(() => import("@splidejs/react-splide").then(mod => mod.Splide), {
   ssr: false,
+  loading: () => <Loading />, // 読み込み中に表示
 });
 
 const SplideSlide = dynamic(
@@ -25,6 +29,7 @@ type AllWorks = {
 }
 
 type Metadata = {
+  num: string
   title: string
   publishedAt: string
   summary: string
@@ -33,9 +38,10 @@ type Metadata = {
 
 const DisplayPosts = ({ allWorks }: WorkListProps) => {
   return (
-    <div className="min-w-full flex justify-center overflow-x-hidden overflow-y-visible py-8">
-      <div className="max-w-6xl">
+    <div className="min-w-full h-full flex justify-center overflow-x-hidden overflow-y-visible py-8">
+      <div className="max-w-6xl h-full">
         <Splide
+          className="h-[424px] w-[820px] splide-work"
           aria-label="works-list"
           options={{
             mediaQuery: 'min',
@@ -46,17 +52,9 @@ const DisplayPosts = ({ allWorks }: WorkListProps) => {
             speed: 1000,
             pauseOnFocus: false,
             gap: "1rem",
-            perPage: 4,
+            perPage: 1,
             rewind: true,
-            breakpoints: {
-              600: {
-                perPage: 2,
-              },
-              1025: {
-                perPage: 4,
-                gap: 32,
-              }
-            },
+
           }}
         >
           {allWorks
@@ -68,23 +66,26 @@ const DisplayPosts = ({ allWorks }: WorkListProps) => {
             .map((post) => (
               <SplideSlide key={post.slug}>
                 <Link
-                  className="flex flex-col space-y-1 mb-4"
+                  className="w-full h-[424px] flex flex-col space-y-1 mb-4 border-none"
                   href={`/work/${post.slug}`}
                 >
-                  <article className="border border-transparent rounded-[5px] overflow-hidden shadow-xl/30 shadow-pf-text">
-                    <div className="h-40">
-                      <img className="w-full h-full" src={post.metadata.image} alt="" />
-                    </div>
-                    <div className="relative h-40 flex flex-col md:flex-row space-x-0 px-4 py-3 text-pf-text bg-pf-bg">
-                      <div className="flex flex-col">
-                        <p className="font-bold dark:text-neutral-100 tracking-tight mb-2 line-clamp-2">
-                          {post.metadata.title}
-                        </p>
-                        <p className="text-[12px] line-clamp-3">{post.metadata.summary}</p>
+                  <article className="w-full h-full rounded-[5px] overflow-hidden shadow-xl/30 shadow-pf-text group">
+                    <div className="w-full h-full relative">
+                      <div className="absolute bottom-0 left-0 z-20 h-14 w-full bg-pf-works-line px-6 flex items-center justify-between">
+                        <div className="flex items-center gap-3 h-full">
+                          <FaReact className="fill-[#30C7EC] w-8 h-8" />
+                          <SiNextdotjs className="fill-[#FFF] w-8 h-8" />
+                        </div>
+                        <p className="text-pf-bg text-[12px] font-light">木乃宮 ダイ</p>
                       </div>
-                      <p className="absolute text-[12px] font-medium bottom-3 right-4 dark:text-neutral-400 tabular-nums">
-                        {post.metadata.publishedAt}
-                      </p>
+                      <div className="absolute -top-12 -left-12 px-18 py-18 z-30 w-0 h-0 group-hover:h-[calc(100%-8px)] group-hover:w-[calc(100%+48px)] bg-black/70 rounded-[30px] transition-all duration-1000 ease-in-out overflow-hidden">
+                        <div className="absolute bottom-0 right-0 w-24 h-24 flex items-center justify-center"><span className="text-pf-bg text-4xl">{post.metadata.num}</span></div>
+                        <h3 className="text-pf-bg text-3xl font-black transition-all opacity-0 duration-1000 ease-in-out overflow-hidden whitespace-nowrap group-hover:opacity-100">{post.metadata.title}</h3>
+                        <div className="min-w-full">
+                          <p className="text-pf-bg font-medium transition-all opacity-0 mt-4 duration-1000 ease-in-out overflow-hidden group-hover:opacity-100 line-clamp-2">{post.metadata.summary}</p>
+                        </div>
+                      </div>
+                      <img className="w-full h-full absolute top-0 left-0 object-cover z-10" src={post.metadata.image} alt="" />
                     </div>
                   </article>
                 </Link>
